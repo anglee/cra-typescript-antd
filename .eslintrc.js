a82e9b89be63cfb3d975c818cf62940ecc362102
Ang Lee <ang.lee@gmail.com>
2019-07-26 16:47:17 -0400

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+
+  extends: [
+    "airbnb",
+    // "eslint:recommended",
+    // "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+  ],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+    "spaced-comment": ["error", "always", { markers: ["/"] }],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
