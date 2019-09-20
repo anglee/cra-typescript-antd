0e50b33b08c2866f80bfe332ba6458ba9f8d1070
Ang Lee <ang.lee@gmail.com>
2019-09-20 17:02:42 -0400

    Add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { Button } from "antd";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App: React.FC = (): JSX.Element => (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+      <Button
+        type="primary"
+        onClick={() => {
+          console.log("Clicked Test"); // eslint-disable-line no-console
+        }}
+      >
+        Test
+      </Button>
+    </header>
+  </div>
 );
 
 export default App;
