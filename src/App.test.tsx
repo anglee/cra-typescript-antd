6bb76a7ad63b12d6e750d7808a44957de4f1ec3f
Ang Lee <ang.lee@gmail.com>
2020-06-22 09:50:56 -0400

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+//jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+https: Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  // expect(true).toBe(true);
 });
